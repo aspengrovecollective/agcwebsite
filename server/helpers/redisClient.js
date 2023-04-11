4a0c3e5c19164f543b2ce4d28eb3dcd138462c00
Haridas Nykiel <haridasnykiel@gmail.com>
2023-04-11 22:00:24 +0100

    updated the redis client to encapsulate the redis client creation. Also updated the client to use the .env file.

--- server/helpers/redisClient.js
+++ server/helpers/redisClient.js
@@ -1,28 +1,34 @@
 const redis = require('redis');
 const { v4: uuidv4 } = require('uuid');
 const { promisifyAll } = require('bluebird');
+require('dotenv').config();
 
 promisifyAll(redis);
 
-const redisPort = 6379; // TODO add these values to config file
-const redisHost = '127.0.0.1';
-
-const createClientAsync = async () =>
-    redis.createClient({
-        socket: {
-            port: redisPort,
-            host: redisHost,
-        },
-    });
+const redisHost = process.env.REDIS_HOST;
+const redisPort = process.env.REDIS_PORT;
 
 class RedisClient {
     instance = null;
+    redisPort;
+    redisHost;
 
-    constructor() {
+    constructor(redisPort, redisHost) {
+        this.redisPort = redisPort;
+        this.redisHost = redisHost;
+
         if (this.instance == null) {
-            this.instance = createClientAsync();
+            this.instance = this.#createClientAsync();
         }
     }
+
+    #createClientAsync = async () =>
+        redis.createClient({
+            socket: {
+                port: redisPort,
+                host: redisHost,
+            },
+        });
 
     async getAsync() {
         const client = await this.instance;
@@ -43,7 +49,7 @@ class RedisClient {
     }
 }
 
-const redisClient = new RedisClient();
+const redisClient = new RedisClient(redisPort, redisHost);
 
 // These functions should go into there own file, which specifically handles the building of the ref codes and saving them to redis, with the count.
 // Or get them from redis.
@@ -86,7 +92,7 @@ const updateRefCodeCountAsync = async (refCodeKey) => {
 
     const { url, visitCount } = JSON.parse(refCode);
     const newVisitCount = visitCount + 1;
-    const updatedRefCode = JSON.stringify({ url, newVisitCount });
+    const updatedRefCode = JSON.stringify({ url, visitCount: newVisitCount });
     await client.set(refCodeKey, updatedRefCode);
     return newVisitCount;
 };
